fd364d4cd1c20291e718a17c1e6cf9f564220af9
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-06-27 01:55:16 +0300

    feat: add sidebar component

--- apps/www/src/components/Product.tsx
+++ apps/www/src/components/Product.tsx
@@ -1,12 +1,16 @@
 import type { FC } from 'react';
 import SectionContainer from '@/components/containers/SectionContainer';
+import SideBar from '@/components/sidebar';
 
 interface ProductProps { }
 
 const Product: FC<ProductProps> = ({ }) => {
   return (
-    <SectionContainer asChild>
-      Product
+    <SectionContainer asChild className='flex gap-[60px]'>
+      <SideBar />
+      <div>
+        <h2>Arrows</h2>
+      </div>
     </SectionContainer>
   );
 }
