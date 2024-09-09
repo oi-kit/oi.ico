57df33a415db25168ffb247bf34b4ee819a9f9b3
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-09-09 23:18:02 +0300

    refactor: change Button styles for ThemeToggle and add icons

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -3,7 +3,14 @@
 import type { FC, ComponentProps } from 'react';
 
 import { Button } from '@/components/primitives/button';
+
 import { useTheme } from 'next-themes';
+
+import SVG from '@/components/svg';
+
+import { MOON, SUN } from '@/config/icons';
+
+import { cn } from '@/utils/cn';
 
 const ThemeToggle: FC<ComponentProps<'button'>> = ({ className, ...rest }) => {
   const { theme, setTheme } = useTheme()
@@ -17,8 +24,26 @@ const ThemeToggle: FC<ComponentProps<'button'>> = ({ className, ...rest }) => {
   };
 
   return (
-    <Button onClick={() => toggleTheme()}>
-      {theme === 'light' ? '🌞' : '🌚'}
+    <Button
+      className={cn(
+        'bg-transparent hover:bg-transparent',
+        'border-none p-0',
+        className
+      )} onClick={() => toggleTheme()} {...rest}>
+      {theme === 'light'
+        ? <SVG
+          className='size-6'
+          stroke='currentColor'
+          fill='none'
+          d={MOON}
+        />
+        : <SVG
+          className='size-6'
+          stroke='currentColor'
+          fill='none'
+          d={SUN}
+        />
+      }
     </Button>
   );
 };
