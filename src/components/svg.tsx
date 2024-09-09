184b9a1a464019e4180cc3a5da0557098a68f9ba
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-09-09 22:46:24 +0300

    feat: add SVG component

---
+++ src/components/svg.tsx
@@ -0,0 +1,44 @@
+import type { FC, ComponentProps } from 'react';
+
+import { cn } from '@/utils/cn';
+
+const SVG: FC<ComponentProps<'svg'>> = ({
+  d,
+  fill = 'currentColor',
+  width,
+  height,
+  className,
+  fillRule = 'evenodd',
+  clipRule = 'evenodd',
+  stroke,
+  viewBox = '0 0 24 24',
+  strokeWidth,
+  strokeLinecap,
+  strokeLinejoin,
+  ...rest
+}) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
+      viewBox={viewBox}
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      className={cn(className)}
+      {...rest}
+    >
+      <path
+        d={d}
+        fill={fill}
+        fillRule={fillRule}
+        clipRule={clipRule}
+        strokeLinecap={strokeLinecap}
+        strokeLinejoin={strokeLinejoin}
+      />
+    </svg>
+  );
+};
+
+export default SVG;
