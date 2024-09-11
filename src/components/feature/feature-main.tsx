6c14057ea93d4e337f95c00ee45d99249ea8dd43
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-09-11 21:26:48 +0300

    feat: add skeleton for feature main dialog

--- src/components/feature/feature-main.tsx
+++ src/components/feature/feature-main.tsx
@@ -1,9 +1,29 @@
+'use client';
+
 import type { ComponentProps, FC } from 'react';
+
+import { Button } from '@/components/primitives/button';
+
+import { Dialog, DialogContainer, DialogContent, DialogTrigger } from '@/components/primitives/dialog';
 
 const FeatureMain: FC<ComponentProps<'div'>> = ({ className, ...rest }) => {
   return (
-    <div>
-      FeatureMain
+    <div className='space-y-4' {...rest}>
+      <h2 className='text-2xl'>Icon</h2>
+      <div className='grid grid-cols-[repeat(auto-fill,minmax(4rem,1fr))] gap-6'>
+        <Dialog>
+          <Button asChild variant='icon' size='icon'>
+            <DialogTrigger>
+              Icon
+            </DialogTrigger>
+          </Button>
+          <DialogContainer>
+            <DialogContent>
+              Icon
+            </DialogContent>
+          </DialogContainer>
+        </Dialog>
+      </div>
     </div>
   );
 };
